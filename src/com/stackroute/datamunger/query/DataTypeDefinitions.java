7a146347c609eb1bc19d6d09a3d1bfd17b226014
subbvam <subbvam@rbs.co.uk>
2022-10-06 21:08:10 +0530

    java3 submitted

--- src/com/stackroute/datamunger/query/DataTypeDefinitions.java
+++ src/com/stackroute/datamunger/query/DataTypeDefinitions.java
@@ -6,8 +6,19 @@ public class DataTypeDefinitions {
 	 * This class should contain a member variable which is a String array, to hold
 	 * the data type for all columns for all data types
 	 */
-
-	public String[] getDataTypes() {
-		return null;
+	String[] dataType;
+	
+	public DataTypeDefinitions(String[] dataType) {
+		this.dataType = dataType;
 	}
+	
+	public String[] getDataTypes() {
+		return dataType;
+	}
+	
+	public void setDataTypes(String[] dataType) {
+		this.dataType = dataType;
+	}
+	
+	
 }
